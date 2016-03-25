bf3647222088ae222c54b2c7e6cd527318abf261
Conrad Stansbury <chstansbury@gmail.com>
2016-03-24 20:51:28 -0700

    Update example with gameplay component

--- example/src/example.js
+++ example/src/example.js
@@ -1,19 +1,22 @@
 /* eslint-disable react/jsx-no-bind, no-console */
 const React = require('react');
 const ReactDOM = require('react-dom');
-const { BadukBoard, Piece } = require('react-baduk');
+const { Baduk, BadukBoard, Piece } = require('react-baduk');
 
 const App = React.createClass({ // eslint-disable-line
   render() {
     return (
-      <section>
-        <BadukBoard size={19} labelStyle="hybrid"
-          onClickEmpty={(x, y) => console.log(`empty ${x} ${y}`)}
-        >
-          <Piece x={4} y={7} color="white" onClick={() => console.log('white')} />
-          <Piece x={0} y={0} color="black" onClick={() => console.log('black')} />
-        </BadukBoard>
-      </section>
+      <article>
+        <Baduk size={19} />
+        <section>
+          <BadukBoard size={19} labelStyle="hybrid"
+            onClickEmpty={(x, y) => console.log(`empty ${x} ${y}`)}
+          >
+            <Piece x={4} y={7} color="white" onClick={() => console.log('white')} />
+            <Piece x={0} y={0} color="black" onClick={() => console.log('black')} />
+          </BadukBoard>
+        </section>
+      </article>
     );
   },
 });
